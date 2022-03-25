3873d8ca4dcfff3c8558a1f728783ab6526e09a4
pablofsc <pablofsc@outlook.com>
2022-03-25 20:36:23 -0300

    Faz o programa funcionar

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ var mc;
 
 window.onload = function () {
     document.getElementById("criptografar").addEventListener("click", () => {
-        m = prompt("Digite a mensagem a ser criptogradada");
-        k = prompt("Digite a chave a ser usada");
+        do {
+            m = prompt("Digite a mensagem a ser criptogradada");
+            k = prompt("Digite a chave a ser usada");
+        } while (k.length > m.length && !alert("A chave precisa ser menor que a mensagem."));
 
         mc = criptografar(m, k);
     });
@@ -28,23 +30,24 @@ function criptografar(mensagem, chave) {
         j++;
     }
 
-    alert(mensagemCriptografada);
+    alert(mensagemCriptografada.join(" "));
 
     return (mensagemCriptografada);
 }
 
-function desc(mensagem, chave) {
+function desc(mensagemCriptografada, chave) {
     let mensagemDescriptografada = [];
     let j = 0;
 
-    for (i = 0; i < mensagem.length; i++) {
+    for (i = 0; i < mensagemCriptografada.length; i++) {
         if (j >= chave.length) {
             j = 0;
         }
-        mensagemDescriptografada.push(String.fromCharCode(mensagem[i] ^ chave[j]));
+        let charCode = mensagemCriptografada[i] ^ chave[j].charCodeAt(0);
+        mensagemDescriptografada.push(String.fromCharCode(charCode));
         j++;
     }
 
-    alert(mensagemDescriptografada);
+    alert(mensagemDescriptografada.join(""));
 }
 
